25e1eef45e69f84fee5721ccc26c7043eb348b47
Bouzkri, Rachid <r.bouzkri@kostal.com>
2026-05-06 14:17:29 +0100

    Automated backup on 06/05/2026 at 14:17:28,99

---
+++ ITSM/backup_to_git.bat
@@ -0,0 +1,40 @@
+@echo off
+color 0B
+echo =======================================================
+echo          ITSM Project Git Backup Utility
+echo =======================================================
+echo.
+
+:: Ensure we are in the script's directory
+cd /d "%~dp0"
+
+echo [1/4] Checking git status...
+git status -s
+echo.
+
+set /p commit_msg="Enter commit message (or press Enter for automated timestamp): "
+
+if "%commit_msg%"=="" (
+    :: Build a safe timestamp string
+    for /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
+    for /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
+    set commit_msg=Automated backup on %date% at %time%
+)
+
+echo.
+echo [2/4] Staging all changes...
+git add .
+
+echo.
+echo [3/4] Committing changes...
+git commit -m "%commit_msg%"
+
+echo.
+echo [4/4] Pushing to remote repository...
+git push
+
+echo.
+echo =======================================================
+echo               Backup Process Finished!
+echo =======================================================
+pause
